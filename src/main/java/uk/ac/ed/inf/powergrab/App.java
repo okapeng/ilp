7b0ddb21f69ef3897920b6d2292d7837bdfab040
s1768094 <s1768094@ed.ac.uk>
2019-09-23 18:09:45 +0100

    project skeleton

--- src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -10,8 +10,9 @@ public class App {
 			return;
 		}
 
-		String date = String.format("http://homepages.inf.ed.ac.uk/stg/powergrab/%d/%d/%d/", Integer.parseInt(args[2]),
-				Integer.parseInt(args[1]), Integer.parseInt(args[0]));
+		String date = String.format("http://homepages.inf.ed.ac.uk/stg/powergrab/%s/%s/%s/powergrabmap.geojson",
+				args[2], args[1], args[0]);
+
 		Position initPos = new Position(Double.parseDouble(args[3]), Double.parseDouble(args[4]));
 		int randomSeed = Integer.parseInt(args[5]);
 		String droneType = args[6];
