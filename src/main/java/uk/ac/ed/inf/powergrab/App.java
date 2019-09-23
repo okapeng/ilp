8fe3a7157280a3132a9cef38f622a1f09989de66
s1768094 <s1768094@ed.ac.uk>
2019-09-23 18:07:34 +0100

    add DroneType

--- src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -6,12 +6,26 @@ public class App {
 			return;
 		}
 
-		String date = String.format("http://homepages.inf.ed.ac.uk/stg/powergrab/%s/%s/%s/powergrabmap.geojson", 
+		String date = String.format("http://homepages.inf.ed.ac.uk/stg/powergrab/%s/%s/%s/powergrabmap.geojson",
 				args[2], args[1], args[0]);
+
 		Position initPos = new Position(Double.parseDouble(args[3]), Double.parseDouble(args[4]));
 		int randomSeed = Integer.parseInt(args[5]);
-		String droneType = args[6];
+		DroneType droneType = DroneType.valueOf(args[6]);
 		
-		
+		//TODO download json map data
+
+		switch (droneType) {
+		case stateful:
+
+			break;
+		case stateless:
+
+			break;
+
+		default:
+			return;
+		}
+
 	}
 }
