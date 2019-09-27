a2b7ef8419011d1b99604c219e350b78408c52ae
wangyuqing <okapeng@163.com>
2019-09-27 12:58:25 +0100

    cw1 add null input handle

--- src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -1,13 +1,13 @@
 package uk.ac.ed.inf.powergrab;
 
 public class Position {
-	
+
 	private static final double R = 0.0003;
 	private static final double MAX_LATITIUDE = 55.946233;
 	private static final double MIN_LATITIUDE = 55.942617;
 	private static final double MAX_LONGITUDE = -3.184319;
 	private static final double MIN_LONGITUDE = -3.192473;
-	
+
 	public double latitude;
 	public double longitude;
 
@@ -17,24 +17,27 @@ public class Position {
 	}
 
 	/**
-	 * Calculate the new Position after moving in a given direction 
+	 * Calculate the new Position after moving in a given direction
+	 * 
 	 * @param direction
 	 * @return valid new position i.e. within the playing area
 	 */
 	public Position nextPosition(Direction direction) {
-		Position newPos = new Position(latitude + R * direction.getLatitudeChange(), 
-				longitude + R * direction.getLongitudeChange());
-		return newPos.inPlayArea() ? newPos : null;
+		if (direction == null) {
+			return null;
+		}
+		double newLatitude = latitude + R * direction.getLatitudeChange();
+		double newLongitude = longitude + R * direction.getLongitudeChange();
+		return new Position(newLatitude, newLongitude);
 	}
 
 	/**
 	 * Check whether the position is within the playing area
+	 * 
 	 * @return
 	 */
 	public boolean inPlayArea() {
-		return this.latitude < MAX_LATITIUDE 
-				&& this.latitude > MIN_LATITIUDE 
-				&& this.longitude < MAX_LONGITUDE
+		return this.latitude < MAX_LATITIUDE && this.latitude > MIN_LATITIUDE && this.longitude < MAX_LONGITUDE
 				&& this.longitude > MIN_LONGITUDE;
 	}
 }
