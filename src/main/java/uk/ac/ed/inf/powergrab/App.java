aebcae017bb8666e044948bcc0c13ea1c2826dfc
Dmitrii Ustiugov <dmitrii.ustiugov@epfl.ch>
2019-10-04 02:47:55 +0100

    local

--- src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -1,12 +1,23 @@
 package uk.ac.ed.inf.powergrab;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.mapbox.geojson.FeatureCollection;
+
+import uk.ac.ed.inf.powergrab.Position;;
+
 public class App {
 	public static void main(String[] args) {
 		if (args.length != 7) {
 			return;
 		}
 
-		String date = String.format("http://homepages.inf.ed.ac.uk/stg/powergrab/%s/%s/%s/powergrabmap.geojson",
+		String mapString = String.format("http://homepages.inf.ed.ac.uk/stg/powergrab/%s/%s/%s/powergrabmap.geojson",
 				args[2], args[1], args[0]);
 
 		Position initPos = new Position(Double.parseDouble(args[3]), Double.parseDouble(args[4]));
@@ -27,5 +38,35 @@ public class App {
 			return;
 		}
 
+		try {
+			HttpURLConnection conn = getConnection(mapString);
+			String mapSource = getMapSource(conn);
+			FeatureCollection features = FeatureCollection.fromJson(mapSource);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static HttpURLConnection getConnection(String mapString) throws Exception {
+		URL mapUrl = null;
+		HttpURLConnection conn = null;
+		mapUrl = new URL(mapString);
+		conn = (HttpURLConnection) mapUrl.openConnection();
+		conn.setReadTimeout(10000); 
+		conn.setConnectTimeout(15000); 
+		conn.setRequestMethod("GET");
+		conn.setDoInput(true);
+		conn.connect();
+		return conn;
+	}
+
+	private static String getMapSource(HttpURLConnection conn) throws IOException {
+		StringBuffer sb = new StringBuffer();
+		InputStreamReader inputStreamReader = new InputStreamReader(conn.getInputStream());
+		while (inputStreamReader.ready()) {
+			sb.append((char) inputStreamReader.read());
+		}
+		return sb.toString();
 	}
 }
